270e7238f7f19977192a38b8318f758b4f2a456b
Chaitra Sure <chaitrasure@gmail.com>
2025-07-24 09:39:45 -0700

    added navigation with text and modifications

--- navigation/ContentView.swift
+++ navigation/ContentView.swift
@@ -9,16 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            
+            VStack {
+                Text("This is the root view 🌿")
+                    .font(.largeTitle)
+                
+                NavigationLink(destination: Text("you've arrived at a second view!")
+                    .fontWeight(.semibold)
+                    .font(.title)) {
+                    Text("click  me!")
+                        .fontWeight(.semibold)
+                }
+                
+            }
+            .padding()
+            
         }
-        .padding()
     }
 }
-
 #Preview {
     ContentView()
 }
